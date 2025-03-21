25082bf1c6654a0bd958ff4a43366667266e4933
BrayanStivenMaldonado <stivenmaldonado0805@gmail.com>
2025-03-21 12:53:39 -0500

    Feat :lipstick: Login view creation

--- VeterinariaJB/src/main/java/module-info.java
+++ VeterinariaJB/src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.example.veterinariajb {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires java.desktop;
+    requires java.logging;
 
 
     opens com.example.veterinariajb to javafx.fxml;
