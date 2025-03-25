dc0ba9c451697726eaf95ef5ae2ffa821ff26f3f
BrayanStivenMaldonado <stivenmaldonado0805@gmail.com>
2025-03-25 18:30:25 -0500

    Feat :sparkles: Main functionality, Inventary gestion view

---
+++ VeterinariaJB/src/main/java/MVC/util/main.java
@@ -0,0 +1,10 @@
+package MVC.util;
+
+import MVC.vista.vistaLogin;
+
+public class main {
+    public static void main(String[] args) {
+        vistaLogin login = new vistaLogin();
+        login.setVisible(true);
+    }
+}
